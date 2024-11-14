9f4e8bb8829b8690ab3b99feccdb5cd1e7abe62a
quan <dhoangquan1@gmail.com>
2024-11-14 17:12:10 -0500

    Task 11B: Deployed the application to AWS Cloud

--- smile.py
+++ smile.py
@@ -12,23 +12,21 @@ def make_shell_context():
     return {'sqla': sqla, 'sqlo': sqlo, 'db': db, 'Post': Post, 'Tag' : Tag, 'User' : User}
 
 
-def add_tags(*args, **kwargs):
-    query = sqla.select(Tag)
-    if db.session.scalars(query).first() is None:
-        tags = [{'name':'funny'},
-          {'name':'inspiring'},
-          {'name':'true-story'},
-          {'name':'heartwarming'}, 
-          {'name':'friendship'}  ]
-        for t in tags:
-            db.session.add(Tag(name = t['name']))
-        db.session.commit()  
-
 @app.before_request
 def initDB(*args, **kwargs):
     if app._got_first_request:
         db.create_all()
-        add_tags()
+        query = sqla.select(Tag)
+        if db.session.scalars(query).first() is None:
+            tags = [{'name':'funny'},
+            {'name':'inspiring'},
+            {'name':'true-story'},
+            {'name':'heartwarming'}, 
+            {'name':'friendship'}  ]
+            for t in tags:
+                db.session.add(Tag(name = t['name']))
+            db.session.commit()
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
